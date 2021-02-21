d2d70e673b249cd899dcbf602964ef896077862f
Pial, Hamidur Rahaman <pial0007l@gmail.com>
2021-02-22 01:34:41 +1030

    story getter created

--- .history/destini-challenge-starting-master/lib/story_brain_20210222013201.dart
+++ .history/destini-challenge-starting-master/lib/story_brain_20210222013201.dart
@@ -1,7 +1,6 @@
 import './story.dart';
 
 class StoryBrain {
-  int storyNumber = 0;
   List<Story> _storyData = [
     Story(
         storyTitle:
@@ -33,13 +32,11 @@ class StoryBrain {
         choice1: 'Restart',
         choice2: '')
   ];
-
-  Story getStory() {
-    return _storyData[storyNumber];
-  }
 }
 
 //TODO: Step 23 - Use the storyNumber property inside getStory(), getChoice1() and getChoice2() so that it gets the updated story and choices rather than always just the first (0th) one.
+
+//TODO: Step 8 - Create a method called getStory() that returns the first storyTitle from _storyData.
 
 //TODO: Step 11 - Create a method called getChoice1() that returns the text for the first choice1 from _storyData.
 
